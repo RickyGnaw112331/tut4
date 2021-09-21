57d73bb6cfb7799e5b382dbf4c7911a364197468
Ricky <360140479@qq.com>
2021-09-21 10:23:38 +0800

    First commit

---
+++ src/test/java/cn/ricky/tut4/CalcTest.java
@@ -0,0 +1,16 @@
+package cn.ricky.tut4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+    Calc calc = new Calc();
+
+    @Test
+    void add() {
+        int a = (int)(Math.random());
+        int b = (int)(Math.random());
+        assertEquals(calc.add(a,b),a+b);
+    }
+}
